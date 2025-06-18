a15deeffab2eefbef19478a6264ca806a3a291ad
Joao Victor <vektor5665@hotmail.com>
2025-06-18 19:59:29 -0300

    layout

---
+++ frontend/src/app/cadastro/categoria/layout.tsx
@@ -0,0 +1,19 @@
+import type React from "react"
+import { DashboardLayout } from "@/components/layout/dashboard-layout"
+import { ProtectedRoute } from "@/components/auth/protected-routes"
+
+export default function InsumosLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ProtectedRoute>
+      <DashboardLayout>{children}</DashboardLayout>
+    </ProtectedRoute>
+  )
+}
+export const metadata = {
+  title: "Cadastro de categoria",
+  description: "Página para cadastrar novas categorias no sistema",
+}
